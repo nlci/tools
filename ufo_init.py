b07196ce849c8fc70b708504a61c8b1755f00e64
Bobby de Vos <bobby_devos@sil.org>
2018-05-09 12:59:07 -0600

    Customize for different Latin fonts
    Work done on 24 Apr 2018

--- ufo_init.py
+++ ufo_init.py
@@ -7,11 +7,31 @@ import sys
 ufo = sys.argv[1]
 font = OpenFont(ufo)
 
+# Latin mappings
+latins = {
+    'KAN Badami': 'gentium',
+    'KAN Kaveri': 'exo'
+    }
+
+xheights = {
+    'gentium': 930,
+    'exo': 1096
+    }
+
+capheights = {
+    'gentium': 1260,
+    'exo': 1509
+    }
+
+latin = latins[font.info.familyName]
+xheight = xheights[latin]
+capheight = capheights[latin]
+
 # Modify UFO
 if font.info.xHeight is None:
-    font.info.xHeight = 930
+    font.info.xHeight = xheight
 if font.info.capHeight is None:
-    font.info.capHeight = 1500
+    font.info.capHeight = capheight
 
 # Save UFO
 font.changed()
