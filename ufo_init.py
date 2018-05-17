8cc12e68b0eb5b84c4cb6ba3c9f33ecd51197b74
Bobby de Vos <bobby_devos@sil.org>
2018-05-16 20:59:16 -0600

    Keep original UPM from the legacy fonts

--- ufo_init.py
+++ ufo_init.py
@@ -12,30 +12,34 @@ latins = {
     # For deva, a Latin font has not been selected
 
     # deva
-    'Panini': 'gentium',
-    'Kautilya': 'gentium',
-    'Maurya': 'gentium',
+    'Panini': 'gentium2048',
+    'Kautilya': 'gentium2048',
+    'Maurya': 'gentium1000',
 
     # knda
-    'KAN Badami': 'gentium',
-    'KAN Kaveri': 'exo',
+    'KAN Badami': 'gentium1000',
+    'KAN Kaveri': 'exo1000',
 
     # taml
-    'ThiruValluvar': 'gentium',
-    'Auvaiyar': 'charis',
-    'Vaigai': 'gentium'
+    'ThiruValluvar': 'gentium2048',
+    'Auvaiyar': 'charis1000',
+    'Vaigai': 'gentium1000'
     }
 
 xheights = {
-    'charis': 987,
-    'gentium': 930,
-    'exo': 1096
+    'charis2048': 987,
+    'charis1000': round(987 * 1000/2048),
+    'gentium2048': 930,
+    'gentium1000': round(930 * 1000/2048),
+    'exo1000': 536
     }
 
 capheights = {
-    'charis': 1374,
-    'gentium': 1260,
-    'exo': 1509
+    'charis2048': 1374,
+    'charis1000': round(1374 * 1000/2048),
+    'gentium2048': 1260,
+    'gentium1000': round(1260 * 1000/2048),
+    'exo1000': 738
     }
 
 latin = latins[font.info.familyName]
