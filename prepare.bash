c2613fd61e655ac64a04e8b08fc36955d0910837
Bobby de Vos <bobby_devos@sil.org>
2018-09-20 16:12:40 -0600

    Check SFD font source files for errors when generating UFOs
    Work done by 29 Aug 2018

--- prepare.bash
+++ prepare.bash
@@ -29,7 +29,22 @@ pushd ${ufo}
 for ttf in *.ttf
 do
     echo $ttf
+
+    # create sfd files
     sfd="$(basename $ttf .ttf).sfd"
     fontforge -script ${nlci}/ttf2sfd.ff $ttf $sfd
 done
+
+# check sfd files
+echo "Checking for duplicate glyphs"
+for sfd in *.sfd
+do
+    grep StartChar $sfd /dev/null | sort | uniq -d
+done
+
+for sfd in *.sfd
+do
+    grep StartChar $sfd | sort > ${sfd}.glyphs
+done
+sha1sum *.glyphs | sort
 popd
