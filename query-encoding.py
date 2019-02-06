a4cd8bbb9ba98450db57ac1dd4e4f7fed927c63f
Bobby de Vos <bobby_devos@sil.org>
2019-02-06 13:28:54 -0700

    Verify glyph names are correctly encoded

---
+++ query-encoding.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+from fontParts.world import *
+import sys
+
+# Open UFO
+ufo = sys.argv[1]
+font = OpenFont(ufo)
+
+# Query UFO
+
+## Find glyphs with encoding issues
+cmap = dict()
+for glyph in font:
+
+    # Only check encoded glyphs
+    if glyph.unicode is None and len(glyph.unicodes) == 0:
+        continue
+
+    # Check for a USV used by another glyph
+    if glyph.unicode in cmap:
+        print(f'glyph {glyph.name} has USV of U+{glyph.unicode:04X}, but that USV is also used for glyph {cmap[glyph.unicode]}')
+
+    # Add codepoint of current glyph
+    cmap[glyph.unicode] = glyph.name
+
+    # Ensure glyph names match encoding (for names starting with u or uni)
+    name = None
+    if glyph.name.startswith('uni'):
+        name = glyph.name[3:]
+    elif glyph.name.startswith('u'):
+        name = glyph.name[1:]
+    try:
+        usv = int(name, 16)
+        if usv != glyph.unicode:
+            print(f'glyph {glyph.name} has incorrect USV of U+{glyph.unicode:04X}')
+    except:
+        pass
+
+    # Ligatures should not be encoded
+    if '.' in glyph.name:
+        print(f'glyph {glyph.name} is a ligature but has an encoding of U+{glyph.unicode:04X}')
+
+    # Check consistency of UFO
+    if len(glyph.unicodes) == 0:
+        print(f'glyph {glyph.name} does not fully specify the USV')
+
+    # Ensure the same USV is specified
+    if glyph.unicode != glyph.unicodes[0]:
+        print(f'glyph {glyph.name} has inconsistent encodings')
+
+    # Check for double encoding (one glyph has multiple USVs)
+    if len(glyph.unicodes) > 1:
+        print(f'glyph {glyph.name} has multiple encodings')
+
+# Modify UFO
+
+# Save UFO
+# font.changed()
+# font.save()
+# font.close()
