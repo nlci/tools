9c9dd0c8bb897571ece03fe772c834eb157fae42
Bobby de Vos <bobby_devos@sil.org>
2022-03-02 15:00:06 -0700

    Correct contour directions for Devanagari and Kannada

--- reverse-fix-direction.py
+++ reverse-fix-direction.py
@@ -12,8 +12,16 @@ mode = sys.argv[2]
 # Modify UFO
 
 all_fixes = {
+    # deva
+    'Kautilya': ['abbreviationsign'],
+    'Maurya': ['comma.deva', 'period.deva', 'candrabindu', 'na.half', 'ra.sub', 'nukta', 'nukta_umatra', 'nukta_uumatra', 'umatra.rphf', 'uumatra.low', 'uumatra.rphf', 'virama', 'fa.half', 'dottedcircle'],
+
+    # knda
+    'Badami': ['uvowel', 'sevenknda'],
+    'Kaveri': ['fa.sub'],
+
     # taml
-    'Auvaiyar': ['nga_iimatra', 'lla_umatra', 'ma_umatra']
+    'Auvaiyar': ['nga_iimatra', 'lla_umatra', 'ma_umatra'],
     }
 fixes = all_fixes.get(font.info.familyName, [])
 
@@ -36,7 +44,8 @@ if mode == 'init':
             glyph.correctDirection()
 
 elif mode == 'import':
-    pass
+    glyph = font['llvocalicmatra']
+    glyph.correctDirection()
 
 # Save UFO
 font.changed()
